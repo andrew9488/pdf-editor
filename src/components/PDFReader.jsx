171f35ea433a9d7b393c57c2bbb5afb623915d80
User <pashkevichandrew@gmail.com>
2021-10-11 10:39:01 +0300

    fix

--- src/components/PDFReader.jsx
+++ src/components/PDFReader.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {Document, Page, pdfjs} from "react-pdf";
 import {ControlPanel} from "./ControlPanel";
 import classes from "./PDFReader.module.css"
@@ -14,18 +14,34 @@ export const PDFReader = () => {
     const [numPages, setNumPages] = useState(null)
     const [pageNumber, setPageNumber] = useState(1)
 
+    const ref = useRef()
+
+    useEffect(() => {
+        let parent = ref.current.parentNode.getBoundingClientRect()
+        document.onmouseup = () => {
+            let select = getSelection().getRangeAt(0).getBoundingClientRect()
+            const x = select.left - parent.left;
+            const y = select.top - parent.top;
+            setPosition({
+                x: x.toFixed(0),
+                y: y.toFixed(0)
+            })
+        };
+    })
+
     const onDocumentLoadSuccess = ({numPages}) => {
         setNumPages(numPages)
     }
 
-    const handleMouseMove = (event) => {
-        setPosition({
-            x: event.clientX,
-            y: event.clientY
-        })
-    }
+    // const handleMouseMove = (event) => {
+    //     setPosition({
+    //         x: event.clientX,
+    //         y: event.clientY
+    //     })
+    // }
 
     // const onClick = (e) => {
+    //     debugger
     //     const parent = e.target.parentNode.getBoundingClientRect();
     //     const element = e.target.getBoundingClientRect();
     //
@@ -33,18 +49,20 @@ export const PDFReader = () => {
     //     const y = element.top - parent.top;
     //     console.log(x, y)
     //     setPosition({
-    //         x: x,
-    //         y: y
+    //         x: x.toFixed(0),
+    //         y: y.toFixed(0)
     //     })
     // }
+
 
     return (
         <div>
             <section
                 id="pdf-section"
                 className={classes.pdfSection}
-                // onClick={onClick}
-                onMouseUp={handleMouseMove}
+                // onClick={handleMouseMove}
+                // onMouseDownCapture={handleMouseMove}
+                ref={ref}
             >
                 <ControlPanel
                     scale={scale}
