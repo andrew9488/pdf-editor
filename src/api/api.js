f4c5f9099c0ca36241d9d5015f75a956bfe43a8d
User <pashkevichandrew@gmail.com>
2021-10-20 10:10:55 +0300

    fix api

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://0.0.0.0:8000/api/v1/',
+    baseURL: 'https://linkedin-parser.fir.by/api/v1/',
     headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
