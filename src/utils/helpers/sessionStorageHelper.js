e3fa8afbc486a8d4994bf032fbabaddf8a93b4d2
User <pashkevichandrew@gmail.com>
2021-10-20 09:59:52 +0300

    fix

--- src/utils/helpers/sessionStorageHelper.js
+++ src/utils/helpers/sessionStorageHelper.js
@@ -11,7 +11,8 @@ export const setEffectsToSessionStorage = (word, color, type) => {
     } else if (effects[word.page] === undefined) {
         effects = {...effects, [word.page]: []}
     }
-    effects[word.page].push(effect)
+    effects[word.page] = [...effects[word.page], effect]
+    effects[word.page] = effects[word.page].map(e => (e.coordinates[0] === effect.coordinates[0] && e.coordinates[1] === effect.coordinates[1] && e.coordinates[2] === effect.coordinates[2] && e.coordinates[3] === effect.coordinates[3]) ? effect : e)
     sessionStorage.setItem('effects', JSON.stringify(effects))
 }
 
