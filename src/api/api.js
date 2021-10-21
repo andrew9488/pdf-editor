8a57295520b122c0ad115e1951a8e49bb07ec25e
User <pashkevichandrew@gmail.com>
2021-10-21 11:40:58 +0300

    fix

--- src/api/api.js
+++ src/api/api.js
@@ -4,14 +4,16 @@ const instance = axios.create({
     baseURL: 'https://linkedin-parser.fir.by/api/v1/',
     withCredentials: false,
     headers: {
-        'Content-Type': 'multipart/form-data; boundary=file',
+        'Content-Type': 'multipart/form-data',
         'Accept': '*/*',
     }
 })
 
 export const pdfApi = {
     sendPdf(file) {
-        return instance.post('converter/convertation-pdf-to-json/', {file})
+        const formData = new FormData()
+        formData.append(`file`, file)
+        return instance.post('converter/convertation-pdf-to-json/', formData)
             .then(response => response.data)
     }
 }
--- src/utils/helpers/highlightText.js
+++ src/utils/helpers/highlightText.js
@@ -3,8 +3,8 @@ import {accuracy} from "./canvasHelper";
 export const highlightText = (context, color, coordinates, scale) => {
     context.fillStyle = color
     const x = coordinates[0] * scale
-    const y = (accuracy - coordinates[3] + 3) * scale
+    const y = (accuracy - coordinates[3] + 5) * scale
     const width = (coordinates[2] - coordinates[0]) * scale
-    const height = (coordinates[3] - coordinates[1]) * scale
+    const height = (coordinates[3]-2 - coordinates[1]) * scale
     context.fillRect(x, y, width, height)
 }
