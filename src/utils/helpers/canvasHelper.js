9e6421a55577eecf20728c5987b2c04f4ff2fbc4
User <pashkevichandrew@gmail.com>
2021-10-21 16:16:39 +0300

    fix

--- src/utils/helpers/canvasHelper.js
+++ src/utils/helpers/canvasHelper.js
@@ -3,7 +3,7 @@ export const accuracy = 839.25
 export function canvasHelper(context, word, scale) {
     if (context) {
         let fontSize = (word.coordinates[3] - word.coordinates[1]) * scale
-        context.font = `${fontSize}px Gill Sans`;
+        context.font = `${fontSize}px sans-serif`;
         let x = word.coordinates[0] * scale
         let y = (accuracy - word.coordinates[1]) * scale
         context.textBaseline = "alphabetic"
