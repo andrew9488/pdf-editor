84a5b307c9b281ee7286da2a9334c20aa135e980
User <pashkevichandrew@gmail.com>
2021-10-21 11:44:58 +0300

    fix

--- src/utils/helpers/lineThroughTextDecoration.js
+++ src/utils/helpers/lineThroughTextDecoration.js
@@ -4,7 +4,7 @@ export const lineThroughTextDecoration = (context, color, coordinates, scale) =>
     context.beginPath()
     context.lineWidth = '1'
     context.strokeStyle = color
-    const y = ((accuracy - (coordinates[3] + coordinates[1]) / 2) + 3)
+    const y = ((accuracy - (coordinates[3] + coordinates[1]) / 2) + 5)
     context.moveTo(coordinates[0] * scale, y * scale)
     context.lineTo(coordinates[2] * scale, y * scale)
     context.stroke()
